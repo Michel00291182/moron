edc0321407b4188357061014302c1ed59a97925e
Leon Jongsma <leon.jongsma@ndcmediagroep.nl>
2013-10-17 19:49:41 +0200

    fklskdaf

--- src/Environment.java
+++ src/Environment.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2005 Davide Brugali, Marco Torchiano
+ffewef * (C) Copyright 2005 Davide Brugali, Marco Torchiano
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
